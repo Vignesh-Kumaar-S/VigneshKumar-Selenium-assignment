e885b743f994d8b969dc8dcd7a7e04359c164879
Vignesh-Kumaar-S <142718033+Vignesh-Kumaar-S@users.noreply.github.com>
2023-09-18 13:52:20 +0530

    Add files via upload

---
+++ Week5/DeleteLead.java
@@ -0,0 +1,60 @@
+package Week5;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DeleteLead extends Baseclass {
+	
+    @Test
+	public void deleteleadcase() throws InterruptedException {
+    	Thread.sleep(3000);
+		// TODO Auto-generated method stub
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("(//div[@class='x-tab-strip-wrap']//li)[2]")).click();
+		//driver.findElement(By.xpath("//input[@name= 'phoneNumber']")).sendKeys("9876543452");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(3000);
+		
+		System.out.println("capturing and viewing first ID....");
+		WebElement firstlead = driver.findElement(By.xpath("//div[contains(@class,'x-grid3-row')]/table/tbody/tr/td/div/a"));
+		String ID = firstlead.getText();
+		System.out.println(firstlead.getText());
+		firstlead.click();
+		
+		System.out.println("Deleting the first ID.....");
+		driver.findElement(By.xpath("//a[text()='Delete']")).click();
+		
+		System.out.println("Searching the deleted ID.....");
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//input[@name = 'id']")).sendKeys(ID);
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(3000);
+		
+		
+		if(driver.findElement(By.xpath("//div[text()='No records to display'] ")).isDisplayed()) 
+		{
+		WebElement message = driver.findElement(By.xpath("//div[text()='No records to display'] "));
+		String MSG = message.getText();
+		System.out.println(MSG);
+		if (MSG.contains("No records to display")) {
+			System.out.println("The record deleted successfully");
+		}
+		}
+		else 
+		{
+			System.out.println("The list of data is shown , Kindly check it");
+		}
+		
+		Thread.sleep(3000);
+		}
+	
+	}
+		
+	
+
+
